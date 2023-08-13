f94008871a5e9086bf9b0ee49bf8fada0efb21d3
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-13 15:06:42 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
+import CostItem from './components/costItem';
+
+
+
+
 
 function App() {
   return (
     <div>
       <h1>React App</h1>
+      <p>Hello</p>
+      <CostItem />
     </div>
   );
 }
---
+++ costItem.js
@@ -0,0 +1,13 @@
+
+
+
+function CostItem(){
+    return(
+    <h2>Some Text</h2>
+    );
+}
+
+export default CostItem;
+
+
+
