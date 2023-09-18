dd713aef9d81571398c6cd5b8764822bad2e8bf3
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-18 22:53:04 +0300

    Add files via upload

--- CostForm.js
+++ CostForm.js
@@ -1,23 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CostForm.css'
 
 const CostForm = () => {
+
+  const [newCostName, setNewCostName] = useState('');  
+  const nameChangeHandler = (e) => {
+    setNewCostName(e.target.value );
+    console.log(newCostName)
+  }
+
+
+  const [newAmout, setNewAmout] = useState('');  
+  const amountChangeHandler = (e) => {
+    setNewAmout(e.target.value);
+    console.log(newAmout)
+  }
+
+  
+  const [newDate, setNewDate] = useState('');  
+  const dateHandler = (e) => {
+    setNewDate(e.target.value);
+    console.log(newDate)
+  }
+
+
   return (
     <form action="">
         <div className='new-const__controls'>
             <div className='new-cost__control'>
                 <label htmlFor="">Название</label>
-                <input type="text" />
+                <input type="text" onChange={nameChangeHandler} />
             </div>
 
             <div className='new-cost__control'>
                 <label htmlFor="">Сумма</label>
-                <input type="number" min='0.01' step='0.01'/>
+                <input type="number" min='0.01' step='0.01' onChange={amountChangeHandler}/>
             </div>
 
             <div className='new-cost__control'>
                 <label htmlFor="">Дата</label>
-                <input type="date" min='2023-01-01'/>
+                <input type="date" min='2023-01-01' onChange={dateHandler}/>
             </div>
 
             <div>
