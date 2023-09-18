3bdaa98791093d7f6ff3ed0c892409cc51b72b49
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-18 20:13:22 +0300

    Add files via upload

--- costItem.js
+++ costItem.js
@@ -1,17 +1,24 @@
 import './costItem.css'
 import CostDate from './costDate'; 
+import { useState } from 'react';
 
 
 function CostItem(props){
 
-    
+    const [description, setDescription] = useState(props.description)
+
+    const changeDescriptionHandler = () => {
+        setDescription('New Description')
+    }
+
     return(    
     <div>
         <div className='cost-item'>
             <CostDate date={props.date}/>
             <div className='cost-item__description'>
-                <h2>{props.description}</h2>
+                <h2>{description}</h2>
                 <div className='cost-item__price'>{props.amount}$</div>
+                <button onClick={changeDescriptionHandler}>Change Description</button>
             </div>   
         </div>
     </div>
